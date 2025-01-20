a139df98ae15f79bf6136ce77fd63a7ae9e12216
crios <caprada@sysman.com.co>
2025-01-20 11:57:41 -0500

    ajuste pagina principal dinamica

--- lib/src/page/home/ContentHome.dart
+++ lib/src/page/home/ContentHome.dart
@@ -2,19 +2,43 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ContentHome extends StatelessWidget {
+  final double posH = 2.4;
+  final double posV = 2;
+  final double sizeCircle = 200.0;
+  final double sizeChildren = 50.0;
+  final double radiosDistance = 250.0;
+  final double postScreemX = 100.0;
+  final double postScreemY = 100.0;
   final List<Map<String, dynamic>> contentItems = [
     {
       "title": "Prefierenos",
-      "imageUrl": "https://img.fotocommunity.com/el-mundo-en-sus-manos-3a6c3dd3-95da-4de4-bb0b-ad79635ca741.jpg?height=100",
+      "leyenda": "Agradecemos la confianza",
+      "imageUrl": "lib/src/img/logoSF.png",
       "content": "Razones para preferirnos...",
       "className": "PreferenosPage",
     },
     {
       "title": "¿Por qué preferirnos?",
-      "imageUrl": "https://img.fotocommunity.com/el-mundo-en-sus-manos-3a6c3dd3-95da-4de4-bb0b-ad79635ca741.jpg?height=100",
+      "leyenda": "Agradecemos la confianza",
+      "imageUrl": "lib/src/img/app_icon.png",
       "content": "Nuestra calidad nos define...",
       "className": "PorQuePreferirnosPage",
     },
+    {
+      "title": "¿Por qué preferirnos?",
+      "leyenda": "Agradecemos la confianza",
+      "imageUrl": "lib/src/img/app_icon.png",
+      "content": "Nuestra calidad nos define...",
+      "className": "PorQuePreferirnosPage",
+    },
+    {
+      "title": "¿Por qué preferirnos?",
+      "leyenda": "Agradecemos la confianza",
+      "imageUrl": "lib/src/img/app_icon.png",
+      "content": "Nuestra calidad nos define...",
+      "className": "PorQuePreferirnosPage",
+    },
+
   ];
 
   ContentHome({Key? key}) : super(key: key);
@@ -27,9 +51,17 @@ class ContentHome extends StatelessWidget {
         child: Stack(
           alignment: Alignment.center,
           children: [
-            CircleAvatar(
-              radius: 60.0,
-              backgroundImage: AssetImage('lib/src/img/logo2.jpg'),
+            Positioned(
+              left: MediaQuery.of(context).size.width / posV - postScreemX,
+              top: MediaQuery.of(context).size.height / posH - postScreemY,
+              child: Container(
+                width: sizeCircle,
+                height: sizeCircle,
+                child: CircleAvatar(
+                  radius: sizeCircle,
+                  backgroundImage: AssetImage('lib/src/img/logo2.jpg'),
+                ),
+              ),
             ),
             ..._generateTentacles(context),
           ],
@@ -40,11 +72,12 @@ class ContentHome extends StatelessWidget {
 
   // Método para generar los círculos hijos ("tentáculos")
   List<Widget> _generateTentacles(BuildContext context) {
-    final double centerRadius = 100.0; // Radio de distancia desde el centro
-    final double itemRadius = 40.0; // Tamaño de los círculos hijos
-    final double angleStep = 360 / contentItems.length; // Ángulo entre cada círculo
+    final double centerRadius = radiosDistance; // Radio para los "tentáculos"
+    final double itemRadius = sizeChildren;    // Radio de los hijos
+    final double angleStep = 360 / contentItems.length; // Ángulo entre cada hijo
     List<Widget> widgets = [];
 
+    // Generar hijos ("tentáculos") alrededor del círculo central
     for (int i = 0; i < contentItems.length; i++) {
       final double angle = angleStep * i; // Ángulo actual en grados
       final double radians = angle * (pi / 180); // Convertir a radianes
@@ -55,25 +88,19 @@ class ContentHome extends StatelessWidget {
 
       widgets.add(
         Positioned(
-          left: (MediaQuery.of(context).size.width / 2) + x - itemRadius,
-          top: (MediaQuery.of(context).size.height / 2) + y - itemRadius,
+          left: (MediaQuery.of(context).size.width / posV) + x - itemRadius,
+          top: (MediaQuery.of(context).size.height / posH) + y - itemRadius,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               GestureDetector(
-                  onTap: () {
-                    _navigateToClass(context, contentItems[i]["className"]);
-                  },
-                  child: Image.network(
-                    contentItems[i]["imageUrl"]?.toString() ?? '',
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Image.asset(
-                        'lib/src/img/logo.png',
-                        fit: BoxFit.cover,
-                      );
-                    },
-                  )
+                onTap: () {
+                  _navigateToClass(context, contentItems[i]["className"]);
+                },
+                child: CircleAvatar(
+                  radius: itemRadius,
+                  backgroundImage: AssetImage(contentItems[i]["imageUrl"] ?? ''),
+                ),
               ),
               const SizedBox(height: 4),
               Text(
